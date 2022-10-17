b4003e34d68d69a50e672f1df02201ccff10b5d4
Filmon M Habtu <42687157+Filsolu93@users.noreply.github.com>
2022-10-17 05:37:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Filmon")
+print("September 20th 2029")
+print("i am singin up for replte's 100 day python challeng !")
+print(
+    "i will be sure to spend some time every day coding along, for a minimam 10 mintes a day. i'll be using  Replit be using, an amizing online IDE  so i can do this for my phone wherever i happen to be. no excuses for not coding for the middle of the field!"
+)
+print("i am feeling happy ")
+print("you can follow my progress at replit.com/@am am filmon probably")
